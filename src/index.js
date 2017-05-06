8efdaaf45fb273222fc889e597ce45b6c0bf0445
James Brumond <james@jbrumond.me>
2017-05-06 16:44:04 -0700

    update for latest version of http-server and router

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ const { contentType } = require('./middleware/content-type');
 const { notFound } = require('./middleware/not-found');
 const { errorHandler } = require('./middleware/error-handler');
 
-const app = exports.app = createServer({ port: 8080 }, () => {
-	console.log('HTTP/1.1 server listening on port 8080...');
-});
+const app = exports.app = createServer();
 
 app.use(requestLogger);
 app.use(contentType('application/json'));
@@ -15,3 +13,7 @@ app.use(app.router({ notFound }));
 app.catch(errorHandler);
 
 require('./endpoints/people/people.endpoint');
+
+app.server.listen(8080, '0.0.0.0', () => {
+	console.log('HTTP/1.1 server listening on port 8080...');
+});
